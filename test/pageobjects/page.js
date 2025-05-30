317c84768d4a523eddda934ca92d3c78741bfc93
suryana-code <msuryana45@gmail.com>
2025-05-30 16:24:51 +0700

    update url to dev

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -2,7 +2,7 @@ import { browser } from "@wdio/globals";
 
 export default class Page {
   open(path) {
-    return browser.url(`https://msuryana-portfolio.pages.dev/${path}`);
+    return browser.url(`https://dev.msuryana-portfolio.pages.dev/${path}`);
     // return browser.url(`http://localhost:3000/${path}`);
   }
 }
